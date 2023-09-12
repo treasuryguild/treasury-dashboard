ca9998dc46e47929970ed4715674c0fd51964b2d
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-09-12 09:00:58 +0200

    Fixed type issue with meshsdk

---
+++ types/meshsdk.d.ts
@@ -0,0 +1,7 @@
+declare module '@meshsdk/react' {
+    export const MeshProvider: any;
+    export const CardanoWallet: any;
+    export const MeshBadge: any;
+    export const useWallet: any;
+ }
+ 
